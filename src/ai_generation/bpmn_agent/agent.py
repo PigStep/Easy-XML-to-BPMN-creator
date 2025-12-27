1b9e9f0ec57e6e743ed65ac5165f71224ebc89ef
VlaDDOS <91902697+PigStep@users.noreply.github.com>
2025-12-27 20:46:51 +0300

    json and promt managers
    refactor: introduce JsonSchemaManager and remove target_schema_name field

--- src/ai_generation/bpmn_agent/agent.py
+++ src/ai_generation/bpmn_agent/agent.py
@@ -1,14 +1,18 @@
 from langgraph.graph import StateGraph, START, END
 from functools import partial
-from src.ai_generation.promt_manager import get_prompt_manager
+from src.ai_generation.managers.promt import get_prompt_manager
+from src.ai_generation.managers.json_schema import get_json_schema_namager
 from .state import BPMNState, getBpmnClient
 from .nodes.plan import plan
 
 promt_manager = get_prompt_manager()
+schema_manager = get_json_schema_namager()
 llm = getBpmnClient()
 
 
-plan_with_config = partial(plan, prompt_manager=promt_manager, llm=llm)
+plan_with_config = partial(
+    plan, prompt_manager=promt_manager, llm=llm, schema_manager=schema_manager
+)
 
 
 # Build workflow
--- src/ai_generation/bpmn_agent/nodes/plan.py
+++ src/ai_generation/bpmn_agent/nodes/plan.py
@@ -1,8 +1,10 @@
 import json
 import logging
+
 from ..state import getBpmnClient, BPMNState
 from src.ai_generation.llm_client import LLMClient
-from src.ai_generation.promt_manager import get_prompt_manager
+from src.ai_generation.managers.promt import get_prompt_manager
+from src.ai_generation.managers.json_schema import get_json_schema_namager
 
 logger = logging.getLogger(__name__)
 
@@ -10,21 +12,25 @@ logger = logging.getLogger(__name__)
 def plan(
     state: BPMNState,
     prompt_manager=None,
+    schema_manager=None,
     llm: LLMClient = None,
 ) -> BPMNState:
+    # Define default
     if llm is None:
         llm = getBpmnClient()
     if prompt_manager is None:
         prompt_manager = get_prompt_manager()
+    if schema_manager is None:
+        schema_manager = get_json_schema_namager()
+
+    # get message
 
     intent = state.get("messages")[0]  # First input
-
-    with open(r"data\bpmn_schemas\plan.json", "r", encoding="utf-8") as file:
-        json_schema = json.load(file)  # FIXME decompose
-
     system_promt = prompt_manager.get_prompt("plan", "system")
     response = llm.generate_response_json_based(
-        prompt=intent, system_promt=system_promt, json_schema=json_schema
+        prompt=intent,
+        system_promt=system_promt,
+        json_schema=schema_manager.get_schema("plan"),
     )
 
     if response is None:
